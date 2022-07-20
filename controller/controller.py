46999c25405255cd5f2bc755b367d58188184b19
Rory Conlon <roryconlon1@gmail.com>
2022-07-20 22:30:21 +0100

    final

--- controller/controller.py
+++ controller/controller.py
@@ -1,3 +1,4 @@
+from tokenize import Number
 from app import app
 from flask import render_template
 from models.shopping_list import people as all_people
@@ -9,3 +10,8 @@ def index():
 @app.route('/order')
 def people():
     return render_template('index.html', title="Order", all_people=all_people )
+
+@app.route('/<index>')
+def  order(index):
+    order_number = int(index)
+    return render_template('order.html', title='Individual Order', orders=all_people[order_number])
